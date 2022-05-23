e8e95b2038a74b56748fd49fc0234fe6c16adc34
Vverner <tonollilucas@yahoo.com>
2022-05-23 13:43:13 -0300

    Adicionando o editor nos post types development e management

--- app/controller/development/config.php
+++ app/controller/development/config.php
@@ -4,7 +4,7 @@
 // ========================================
 $cpt = new VVerner\PostType('Desenvolvimento', 'Desenvolvimento', 'development');
 $cpt->setIcon('dashicons-desktop');
-$cpt->setSupports(['title', 'thumbnail', 'excerpt' ,'content']);
+$cpt->setSupports(['title', 'thumbnail', 'excerpt' ,'editor']);
 $cpt->setPublic(true);
 $cpt->register();
 
