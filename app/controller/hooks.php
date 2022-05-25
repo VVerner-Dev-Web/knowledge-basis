f73f15dce1c7e9e15c5252b3e0738cea077951d5
Vverner <tonollilucas@yahoo.com>
2022-05-25 16:43:04 -0300

    Alterações na home e criação do shortcode slider-recent-posts

--- app/controller/hooks.php
+++ app/controller/hooks.php
@@ -1,1 +1,17 @@
 <?php defined('ABSPATH') || exit('No direct script access allowed');
+
+/*SLIDER POSTS RECENTES HOME*/
+$sc = new VVerner\Shortcode('slider-recent-posts');
+
+
+add_filter('vv_shortcode-slider-recent-posts',function($args){
+    $query = new WP_Query([
+        'post_type' => ['development', 'management'],
+        'order_by'  => 'post_date',
+        'order'     => 'DESC'
+    ]);
+
+    $args['query'] = $query;
+
+    return $args;
+});
--- app/views/page/inicio.php
+++ app/views/page/inicio.php
@@ -1,3 +1,4 @@
+
 <main id="main" class="">
   <div id="content" role="main" class="content-area">
     <section class="section dark" id="section_1747333165">
@@ -14,55 +15,12 @@
                         </div>
                     </div>
             </div>
-            <div id="row-1474111950"
-                class="row large-columns-3 medium-columns- small-columns-1 has-shadow row-box-shadow-2 row-masonry"
-                data-packery-options="{&quot;itemSelector&quot;: &quot;.col&quot;, &quot;gutter&quot;: 0, &quot;presentageWidth&quot; : true}"
-                style="position: relative; height: 350.359px;">
-
-                <div class="col post-item" style="position: absolute; left: 0px; top: 0px;">
-                    <div class="col-inner">
-                        <a href="http://dev.conhecimento.vverner.com/demonstrativo-02/" class="plain">
-                            <div class="box box-text-bottom box-blog-post has-hover">
-                                <div class="box-image">
-                                    <div class="image-cover" style="padding-top:180px;">
-                                        <img width="300" height="169"
-                                            src="http://dev.conhecimento.vverner.com/wp-content/uploads/2022/05/placeholder-2-300x169.jpg"
-                                            data-src="http://dev.conhecimento.vverner.com/wp-content/uploads/2022/05/placeholder-2-300x169.jpg"
-                                            class="attachment-medium size-medium wp-post-image lazy-load-active"
-                                            alt=""
-                                            srcset="http://dev.conhecimento.vverner.com/wp-content/uploads/2022/05/placeholder-2-300x169.jpg 300w, http://dev.conhecimento.vverner.com/wp-content/uploads/2022/05/placeholder-2-1024x576.jpg 1024w, http://dev.conhecimento.vverner.com/wp-content/uploads/2022/05/placeholder-2-768x432.jpg 768w, http://dev.conhecimento.vverner.com/wp-content/uploads/2022/05/placeholder-2-1536x864.jpg 1536w, http://dev.conhecimento.vverner.com/wp-content/uploads/2022/05/placeholder-2.jpg 1600w"
-                                            data-srcset="http://dev.conhecimento.vverner.com/wp-content/uploads/2022/05/placeholder-2-300x169.jpg 300w, http://dev.conhecimento.vverner.com/wp-content/uploads/2022/05/placeholder-2-1024x576.jpg 1024w, http://dev.conhecimento.vverner.com/wp-content/uploads/2022/05/placeholder-2-768x432.jpg 768w, http://dev.conhecimento.vverner.com/wp-content/uploads/2022/05/placeholder-2-1536x864.jpg 1536w, http://dev.conhecimento.vverner.com/wp-content/uploads/2022/05/placeholder-2.jpg 1600w"
-                                            sizes="(max-width: 300px) 100vw, 300px"> </div>
-                                </div>
-                                <div class="box-text text-center">
-                                    <div class="box-text-inner blog-post-inner">
-
-
-                                        <h5 class="post-title is-large ">Demonstrativo 02</h5>
-                                        <div class="is-divider"></div>
-                                        <p class="from_the_blog_excerpt ">Lorem ipsum, dolor sit amet
-                                            consectetur adipisicing elit. Nihil consequatur aspernatur sint,
-                                            incidunt labore magni [...] </p>
-
-
-
-                                    </div>
-                                </div>
-                                <div class="badge absolute top post-date badge-outline">
-                                    <div class="badge-inner">
-                                        <span class="post-date-day">16</span><br>
-                                        <span class="post-date-month is-xsmall">maio</span>
-                                    </div>
-                                </div>
-                            </div>
-                        </a>
-                    </div>
-                </div>
-            </div>
-        </div>  
+            <?php echo do_shortcode('[vv-slider-recent-posts]');?> 
+            
+        </div> 
     </section>
 
-    <section class="section dark" id="section_2038087558">
+   <section class="section dark" id="section_2038087558">
         <div class="bg section-bg fill bg-fill bg-loaded">
         </div>
         <div class="section-content relative">
@@ -103,8 +61,11 @@
                         </a>
                     </div>
                 </div>
-            </div>
+            </div> 
         </section>
+    
+
+       
     </div>
 </main>
 
