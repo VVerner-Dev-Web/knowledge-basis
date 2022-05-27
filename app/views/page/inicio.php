d775bd98250a794fd87db9dc7ff73f1ae0c53804
Vverner <tonollilucas@yahoo.com>
2022-05-27 16:51:00 -0300

    Correção no arquivo main.css e no inicio.php

--- app/views/page/inicio.php
+++ app/views/page/inicio.php
@@ -1,14 +1,14 @@
 
 <main id="main" class="">
   <div id="content" role="main" class="content-area">
-    <section class="section dark" id="section_1747333165">
+    <section class="section dark hero" >
         <div class="bg section-bg fill bg-fill bg-loaded">
         </div>
         <div class="section-content relative">
             <div class="row align-center" id="row-1422824135">
                 <div id="col-339111800" class="col medium-6 small-12 large-6">
                     <div class="col-inner">
-                        <div id="text-2832122463" class="text">
+                        <div class="text text-center">
                                 <p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh
                                     euismod tincidunt ut laoreet dolore magna aliquam erat volutpat.</p>
                             </div>
@@ -20,7 +20,7 @@
         </div> 
     </section>
 
-   <section class="section dark" id="section_2038087558">
+   <section class="section dark section-blog-recents">
         <div class="bg section-bg fill bg-fill bg-loaded"></div>
         <div class="section-content relative">
             <?php VVerner\Views::getInstance()->getView('components/blog_posts'); ?>
