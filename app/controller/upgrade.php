4061a47204885ce18109e23becb1c81d6835a740
LucasTonolli <tonollilucas@yahoo.com>
2022-05-31 11:15:10 -0300

    Ajustes no upgrade.php

--- app/controller/upgrade.php
+++ app/controller/upgrade.php
@@ -4,14 +4,14 @@ add_action('init', function(){
     $updates = [
         [
             'flag'   => 'required_page',
-            'version'  => 18,
+            'version'  => 19,
             'action' => 'create_required_pages'
         ]
     ];
 
     foreach ($updates as $update) :
         $oldVersion = get_option($update['flag'], 0);
-        if ($oldVersion < $update['version']) :
+        if (version_compare($oldVersion, $update['version'], '<')) :
             update_option($update['flag'], $update['version']);
 
             do_action('kb/upgrade/' .$update['action'], $update['flag'], $update['version'], $oldVersion); 
@@ -19,27 +19,26 @@ add_action('init', function(){
     endforeach;
 });
 
-add_action('kb/upgrade/create_required_pages',function(string $flag, int $version, int $oldVersion){
+add_action('kb/upgrade/create_required_pages',function(){
     global $wpdb;
     $pages = [
-        '1' => ['Início']
+         'Início',
     ];
     
     foreach ($pages as $page) :
-            foreach ($page as $title_page):
-                $q = "SELECT post_title FROM {$wpdb->posts} WHERE post_title = %s";
-                $preparado = $wpdb->prepare($q, $title_page);
-                $r         = $wpdb->get_var($preparado);
-                
-                if ($r == $title_page) continue;
-                if (!is_page($title_page)): 
-                    wp_insert_post([
-                        'post_type'     => 'page',
-                        'post_title'    => $title_page,
-                        'post_status'   => 'publish',
-                    ]);
-                endif;
-            endforeach;
+            
+        $q = "SELECT post_title FROM {$wpdb->posts} WHERE post_title = %s";
+        $preparado = $wpdb->prepare($q, $page);
+        $r         = $wpdb->get_var($preparado);
+        
+        if ($r == $page) continue;
+        if (!is_page($page)): 
+            wp_insert_post([
+                'post_type'     => 'page',
+                'post_title'    => $page,
+                'post_status'   => 'publish',
+            ]);
+        endif;
     endforeach;
       
 }, 10, 3);
